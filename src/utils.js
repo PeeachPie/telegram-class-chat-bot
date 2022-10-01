aad35f4b83241ddb0324c898373e344aa01a1204
Ernie R <91326168+PeeachPie@users.noreply.github.com>
2022-10-01 21:29:06 +0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,15 +4,24 @@ function getUsernamesById(ctx, membersId) {
     for (const id of membersId) {
       await ctx.getChatMember(id).then((member) => {
         if (member.user.username) {
-          usernames.push(`@${member.user.username}`);
+          usernames.push(toEscapeMsg(`@${member.user.username}`));
         } else {
-          usernames.push(`[${member.user.first_name}](tg://user?id=${member.user.id})`)
+          usernames.push(toEscapeMsg(`[${member.user.first_name}](tg://user?id=${member.user.id})`))
         }
-      });
+      }).catch((error) => {console.log(error)});
     }
     resolve(usernames);
     // reject(error)
   });
+}
+
+function toEscapeMsg(str) {
+  return str
+      .replace(/_/gi, "\\_")
+      .replace(/-/gi, "\\-")
+      .replace("~", "\\~")
+      .replace(/`/gi, "\\`")
+      .replace(/\./g, "\\.");
 }
 
 module.exports = { getUsernamesById }
