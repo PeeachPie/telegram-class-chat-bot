7b4f6e175d4cf93f2235d32209372a3aeecb90fd
PeeachPie <nestandrem@gmail.com>
2022-10-02 12:41:07 +0500

    bulling deleted

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const membersId = require("./data.json").members;
 const { getUsernamesById } = require('./src/utils.js')
 
-bot.start((ctx) => ctx.reply('Ну здрасьте. Я умею отмечать всех и буллить Димона :)'));
+bot.start((ctx) => ctx.reply('Ну здрасьте. Я умею отмечать всех в этой группе :)'));
 
 bot.help((ctx) => ctx.reply(
   `
 /start - запустить бота
 /help - помощь
 /all - отметить всех участников чата
-/bull_dimon - забуллить димона
    `
 ));
 
@@ -20,9 +19,9 @@ bot.command("all", (ctx) => {
   getUsernamesById(ctx, membersId).then(usernames => ctx.reply(usernames.join(' '), {parse_mode: 'MarkdownV2'}));
 })
 
-bot.command("bull_dimon", (ctx) => {
-  ctx.sendSticker('CAACAgEAAxkBAAEF92ljOBOqhWN5vbHdpU8UwwPKJNnoXwACrwEAAsDr6UcSJTT8iVPXaCoE').then(() => ctx.reply('@y0urn3eon'))
-})
+// bot.command("bull_dimon", (ctx) => {
+//   ctx.sendSticker('CAACAgEAAxkBAAEF92ljOBOqhWN5vbHdpU8UwwPKJNnoXwACrwEAAsDr6UcSJTT8iVPXaCoE').then(() => ctx.reply('@y0urn3eon'))
+// })
 
 bot.launch();
 
